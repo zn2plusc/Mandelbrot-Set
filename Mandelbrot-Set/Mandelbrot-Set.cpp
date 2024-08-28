762e0b800fc626f343c1ce057bda0efa95a89b0d
‎‎‎‎‎‎ <151572419+zn2plusc@users.noreply.github.com>
2024-08-28 10:46:23 +0200

    change und conflict test

--- Mandelbrot-Set/Mandelbrot-Set.cpp
+++ Mandelbrot-Set/Mandelbrot-Set.cpp
@@ -3,8 +3,24 @@
 #include <windows.h>
 using namespace std;
 #pragma comment(lib, "user32")
+void maxsc()
+{
+    HWND Hwnd = GetForegroundWindow();
+    ShowWindow(Hwnd, SW_MAXIMIZE);
+}
+void fullsc()
+{
+    HWND Hwnd = GetForegroundWindow();
+    int x = GetSystemMetrics(SM_CXSCREEN);
+    int y = GetSystemMetrics(SM_CYSCREEN);
+    LONG winstyle = GetWindowLong(Hwnd, GWL_STYLE);
+    SetWindowLong(Hwnd, GWL_STYLE, (winstyle | WS_POPUP | WS_MAXIMIZE) & ~WS_CAPTION & ~WS_THICKFRAME & ~WS_BORDER);
+    SetWindowPos(Hwnd, HWND_TOP, 0, 0, x, y, 0);
+
+}
 int main() {
-    ::SendMessage(::GetConsoleWindow(), WM_SYSKEYDOWN, VK_RETURN, 0x20000000);
+    fullsc();
+    maxsc();
     const size_t limit = 1000, size = 100;
     const char letters[] = " 0O@#?!-o*";
     for (size_t iy = 0; iy <= size; iy++) {
