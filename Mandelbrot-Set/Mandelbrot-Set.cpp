33d37d0e50bfa18254157393545a4d260e3511c0
‎‎‎‎‎‎ <151572419+zn2plusc@users.noreply.github.com>
2024-08-28 10:29:33 +0200

    Fullscreen und ACSII änderungen.

--- Mandelbrot-Set/Mandelbrot-Set.cpp
+++ Mandelbrot-Set/Mandelbrot-Set.cpp
@@ -2,14 +2,11 @@
 #include <iostream>
 #include <windows.h>
 using namespace std;
+#pragma comment(lib, "user32")
 int main() {
-    HWND console = GetConsoleWindow();
-    RECT r;
-    GetWindowRect(console, &r);
-
-    MoveWindow(console, r.left, r.top, 1200, 600, TRUE); // 800 breite, 100 höhe
+    ::SendMessage(::GetConsoleWindow(), WM_SYSKEYDOWN, VK_RETURN, 0x20000000);
     const size_t limit = 1000, size = 100;
-    const char letters[] = " 0@#?!-o*";
+    const char letters[] = " 0O@#?!-o*";
     for (size_t iy = 0; iy <= size; iy++) {
         for (size_t ix = 0, count = 0; ix <= size; ix++, count = 0) {
             complex<double> c(-2.0 + ix * 2.5 / size, 1.15 - iy * 2.3 / size), z(0.0, 0.0);
